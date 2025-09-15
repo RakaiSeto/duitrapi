eb85341c0d364d504b789779a716df3b124a747b
rakaiseto <mamangrakai@gmail.com>
2025-09-16 02:23:21 +0700

    fix(cd): port

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,10 +5,7 @@ module.exports = {
             script: 'node_modules/next/dist/bin/next',
             args: 'start',
             // ... other settings
-            env_production: {
-                NODE_ENV: 'production',
-                PORT: 29006, // <-- Add this line
-            },
+            PORT: 29006
         },
     ],
 };
